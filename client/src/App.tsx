0568c0a3a7280849de246bca02448d503e371f6c
Richard Seroka <r-seroka@outlook.com>
2023-08-29 14:44:45 -0400

    Updated Application logic to allow reload app when cache out of date

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,54 +1,91 @@
 // import React from 'react';
 
 import './App.css';
-// import { Routes, Route, Outlet, Link} from "react-router-dom";
-import { Routes, Route, Link} from "react-router-dom";
-import Sudoku from './Sudoku/Sudoku';
+
+import { Link, Route, Routes } from "react-router-dom";
+import Blackjack from './BlackJack/Blackjack';
 import Navbar from './NavBar/NavBar';
 import Resume from "./Resume/Resume";
-import Blackjack from './BlackJack/Blackjack';
+import Sudoku from './Sudoku/Sudoku';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="sudoku" element={<Sudoku />} />
-        <Route path="resume" element={<Resume />} />
-        <Route path="blackjack" element={<Blackjack />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-    </>
+import Button from 'react-bootstrap/Button';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-  );
+import React, { ReactNode } from 'react';
+
+type AppProperties = {
+
+};
+
+type AppState = {
+  newVersionAvailable: boolean;
+  waitingServiceWorker: ServiceWorker | null;
+};
+
+class App extends React.Component<AppProperties, AppState> {
+
+  constructor(props: AppProperties) {
+    super(props);
+
+    this.state = {
+      newVersionAvailable: false,
+      waitingServiceWorker: null
+    }
+  }
+
+
+  private onApplicationUpdateOnServerDetected(registration: ServiceWorkerRegistration)  {
+    console.error("Detected an update web application on server, popping up toast");
+    this.setState({newVersionAvailable: true, waitingServiceWorker: registration.waiting});
+  }
+
+  private onReloadRequested() {
+    // recipe for application update recognition based upon 
+    // https://dev.to/daniellycosta/showing-new-version-available-notification-on-create-react-app-pwas-3jc9
+    const { waitingServiceWorker } = this.state;
+    waitingServiceWorker && waitingServiceWorker.postMessage({ type: "SKIP_WAITING"});
+    this.setState({newVersionAvailable: false});
+    window.location.reload();
+  }
+
+  componentDidMount() {
+    // recipe for application update recognition based upon 
+    // https://dev.to/daniellycosta/showing-new-version-available-notification-on-create-react-app-pwas-3jc9
+    const registrationConfig = { onUpdate: this.onApplicationUpdateOnServerDetected.bind(this) };
+    serviceWorkerRegistration.register(registrationConfig);
+  }
+  
+
+
+
+  render(): ReactNode {
+    return (
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="sudoku" element={<Sudoku />} />
+          <Route path="resume" element={<Resume />} />
+          <Route path="blackjack" element={<Blackjack />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+        <ToastContainer position="middle-center">
+          <Toast show={this.state.newVersionAvailable}>
+            <Toast.Header closeButton={false}>
+              <h1 className="app__popup-header" >Application Updated</h1>
+            </Toast.Header>
+            <Toast.Body className="app__popup-body">
+              <p>The application has been updated since you last ran it in this browser.  You must reload application to get updated version</p>
+              <Button onClick={this.onReloadRequested.bind(this)}>Reload</Button>
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
+      </div>
+    );
+  }
 }
-
-// function Layout() {
-//   return (
-//     <div>
-//       {/* A "layout route" is a good place to put markup you want to
-//           share across all the pages on your site, like navigation. */}
-//       <nav>
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/sudoku">Sudoku</Link>
-//           </li>
-//         </ul>
-//       </nav>
-
-//       <hr />
-
-//       {/* An <Outlet> renders whatever child route is currently active,
-//           so you can think about this <Outlet> as a placeholder for
-//           the child routes we defined above. */}
-//       <Outlet />
-//     </div>
-//   );
-// }
 
 function Home() {
   return (
