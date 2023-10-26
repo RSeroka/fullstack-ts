2c6511068e6aa5bf18d3b69847aaaa392d07fb3b
Richard Seroka <r-seroka@outlook.com>
2023-10-26 17:16:26 -0400

    Finished the BlackJack component

--- client/src/BlackJack/components/BlackjackComp.tsx
+++ client/src/BlackJack/components/BlackjackComp.tsx
@@ -24,12 +24,19 @@ type BlackjackCompState = {
     houseRulesState: string;
     houseRules: undefined | HouseRules;
     strategyState: string;
+    howManyHands: number | undefined;
     strategy: undefined | Strategy;
     playManyHandsState: string;
     strategyResults: undefined | StrategyResults;
 }
 
 class BlackjackComp extends React.Component<BlackjackCompProperties, BlackjackCompState> {
+
+    private loadingDefaultHouseRules:boolean;
+    private loadingDefaultStrategy:boolean;
+    private static defaultHouseRules: undefined | HouseRules;
+    private static defaultStrategy: undefined | Strategy;
+
 
 
     constructor(props: BlackjackCompProperties) {
@@ -38,41 +45,85 @@ class BlackjackComp extends React.Component<BlackjackCompProperties, BlackjackCo
             houseRulesState: "not retrieved",
             strategyState: "not retrieved",
             playManyHandsState: "not retrieved",
-            houseRules: undefined,
-            strategy: undefined,
+            howManyHands: 100000,
+            houseRules: structuredClone(BlackjackComp.defaultHouseRules),
+            strategy: structuredClone(BlackjackComp.defaultStrategy),
             strategyResults: undefined
         }
+        this.loadingDefaultHouseRules = false;
+        this.loadingDefaultStrategy = false;
+    }
 
+    componentDidMount(): void {
+        if (!this.loadingDefaultHouseRules) {
+            if (BlackjackComp.defaultHouseRules !== undefined) {
+                this.onUpdatedHouseRules(structuredClone(BlackjackComp.defaultHouseRules));
+            }
+            else if (this.state.houseRules === undefined) {
+                this.onHouseRulesButtonClick();
+            }
+        }
+
+        if (!this.loadingDefaultStrategy) {
+            if (BlackjackComp.defaultStrategy !== undefined) {
+                this.onUpdatedStrategy(structuredClone(BlackjackComp.defaultStrategy));
+            }
+            else if (this.state.strategy === undefined) {
+                this.onStrategyButtonClick();
+            }
+        }
     }
 
     private onHouseRulesButtonClick() {
-        this.setState({houseRulesState: "in progress"});
-        getAllBlackjackHouseRules().then((value: HouseRules[])=> {
-            this.onUpdatedHouseRules(value[0]);
-
-        }, (reasonRejected: any) => {
-            this.setState({houseRulesState: reasonRejected, houseRules: undefined});
-        })
+        if (BlackjackComp.defaultHouseRules !== undefined) {
+            this.onUpdatedHouseRules(structuredClone(BlackjackComp.defaultHouseRules));
+        }
+        else {
+            this.setState({houseRulesState: "in progress"});
+            this.loadingDefaultHouseRules = true;
+            getAllBlackjackHouseRules().then((value: HouseRules[])=> {
+                this.loadingDefaultHouseRules = false;
+                BlackjackComp.defaultHouseRules = structuredClone(value[0]);
+                this.onUpdatedHouseRules(value[0]);
+            }, (reasonRejected: any) => {
+                this.loadingDefaultHouseRules = false;
+                this.setState({houseRulesState: reasonRejected, houseRules: undefined});
+            });
+        }
     }
 
     private onStrategyButtonClick() {
-        // this.context.setStrategy = this.onUpdatedStrategy.bind(this);
+        if (BlackjackComp.defaultStrategy !== undefined) {
+            this.onUpdatedStrategy(structuredClone(BlackjackComp.defaultStrategy));
+        }
+        else {
+            this.setState({strategyState: "in progress"});
+            this.loadingDefaultStrategy = true;
+            getStrategyById("basicDealerHitsOnSoft17").then((value: Strategy) => {
+                this.loadingDefaultStrategy = false;
+                BlackjackComp.defaultStrategy = structuredClone(value);
+                this.onUpdatedStrategy(value);
+            }, (reasonRejected: any) => {
+                this.loadingDefaultStrategy = false;
+                this.setState({strategyState: reasonRejected, strategy: undefined});
+            });            
+        }
 
-        this.setState({strategyState: "in progress"});
-        
-        getStrategyById("basicDealerHitsOnSoft17").then((value: Strategy) => {
-            this.onUpdatedStrategy(value);
 
-        }, (reasonRejected: any) => {
-            this.setState({strategyState: reasonRejected, strategy: undefined});
-        });
     }
 
     private onPlayManyHandsButtonClick() {
         this.setState({playManyHandsState: "in progress"});
 
+        let howManyHands = this.state.howManyHands;
+        if (this.state.howManyHands === undefined || !Number.isInteger(this.state.howManyHands) 
+            || this.state.howManyHands < 1 || this.state.howManyHands > 100000) {
+                howManyHands = 100000; 
+                this.setState({"howManyHands": howManyHands}); 
+        }
+
         const parameters: PlayManyHandsParams = {
-            numHands: 100000, 
+            numHands: howManyHands!, 
             playerStrategy: this.state.strategy!,
             houseRulesOverride: this.state.houseRules!        
         }
@@ -106,44 +157,68 @@ class BlackjackComp extends React.Component<BlackjackCompProperties, BlackjackCo
         this.setState({houseRulesState: houseRulesString, houseRules: newHouseRules});
     };
 
+    private onHowManyTimesChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const updatedValue = Number.parseInt(event.target.value);
+        if (updatedValue >= 1 && updatedValue <= 100000) {
+            this.setState({howManyHands: updatedValue});
+        }
+        else if (isNaN(updatedValue)) {
+            this.setState({howManyHands: undefined})
+        }
+    }
+
     render(): ReactNode {
+        const playManyHandsPrompt = this.state.strategyResults === undefined? "Run Strategy" : "Rerun Strategy";
+
         return (
-            <div>
-            <button type="button" onClick={(ev) => this.onHouseRulesButtonClick()}>get House Rules</button>
-            <button type="button" onClick={(ev) => this.onStrategyButtonClick()}>get Strategy</button>
-            <button type="button" onClick={(ev) => this.onPlayManyHandsButtonClick()}>play many hands</button>
-            {/* <StrategyResultsComp strategyResults={this.state.strategyResults} ></StrategyResultsComp> */}
-            <Accordion defaultActiveKey={['0']} alwaysOpen>
+            <>
+            
+            <Accordion defaultActiveKey={['0', '1', '2']} alwaysOpen>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Strategy Results&nbsp;
+                        <span className="blackjack__accordian-button" onClick={(ev) => {ev.stopPropagation(); this.onPlayManyHandsButtonClick()}}>
+                            {playManyHandsPrompt}</span>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        <div className="blackjack__runstrategy-params">
+                            <label className="blackjack__runstrategy-params__description">How Many Hands:&nbsp;</label>
+                            <div className="blackjack__runstrategy-params__input-control">
+                                <input className="blackjack__runstrategy-params__input-number" type="number" max={100000} min={1} step={1}
+                                    value={this.state.howManyHands} 
+                                    onChange={this.onHowManyTimesChange.bind(this)} /> 
+                            </div>
+                        </div>
+                        <StrategyResultsComp strategyResults={this.state.strategyResults} ></StrategyResultsComp>
+                    </Accordion.Body>
+                </Accordion.Item>
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>House Rules Configuration</Accordion.Header>
+                    <Accordion.Header>House Rules Configuration&nbsp;
+                        <span className="blackjack__accordian-button" onClick={(ev) => {ev.stopPropagation(); this.onHouseRulesButtonClick()}}>Reload Defaults</span>
+                    </Accordion.Header>
                     <Accordion.Body>
                         <HouseRulesComp setHouseRules={this.onUpdatedHouseRules.bind(this)} 
                             houseRules={this.state.houseRules!}></HouseRulesComp>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header>Strategy Configuration</Accordion.Header>
+                    <Accordion.Header>Strategy Configuration&nbsp;
+                        <span className="blackjack__accordian-button" onClick={(ev) => {ev.stopPropagation(); this.onStrategyButtonClick()}}>Reload Defaults</span>
+                    </Accordion.Header>
                     <Accordion.Body>
                         <StrategyComp setStrategy={this.onUpdatedStrategy.bind(this)}
                             strategy={this.state.strategy} ></StrategyComp>
                     </Accordion.Body>
                 </Accordion.Item>
-                <Accordion.Item eventKey="2">
-                    <Accordion.Header>Strategy Results</Accordion.Header>
-                    <Accordion.Body>
-                        <StrategyResultsComp strategyResults={this.state.strategyResults} ></StrategyResultsComp>
-                    </Accordion.Body>
-                </Accordion.Item>
+
             </Accordion>
-            {/* <HouseRulesComp setHouseRules={this.onUpdatedHouseRules.bind(this)} houseRules={this.state.houseRules!}></HouseRulesComp>
-            <StrategyResultsComp strategyResults={this.state.strategyResults} ></StrategyResultsComp> */}
 
 
-            <p>houseRules: {this.state.houseRulesState}</p>
+
+            {/* <p>houseRules: {this.state.houseRulesState}</p>
             <p>strategy: {this.state.strategyState}</p>
-            <p>strategyResults: {this.state.playManyHandsState}</p>
+            <p>strategyResults: {this.state.playManyHandsState}</p> */}
 
-            </div>
+            </>
         )
     }
 }
